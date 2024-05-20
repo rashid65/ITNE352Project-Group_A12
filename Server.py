7ea197d403e011282301b0378f13115810d0fee9
as <alisalam200jks@gmail.com>
2024-05-20 21:09:12 +0300

    menu general flow ADDED

--- Server.py
+++ Server.py
@@ -1,13 +1,12 @@
 import socket
 import threading
 
-def ServeClient(Sock_a, SockName):
+def ServeClient(Sock_a, SockName): # Thread 
     print("connected to ", SockName , "\n")
     try:
         ClientName = Sock_a.recv(1024).decode('ascii')
         print("the client name is ", ClientName)
-        msg = f'Heloo {ClientName} did you get my message?'
-        Sock_a.sendall(msg.encode('ascii'))
+        
     finally:
         Sock_a.close()
         print("Connection closed with", ClientName)
